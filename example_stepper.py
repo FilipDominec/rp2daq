43a0d27d0d767f97757bf8b91b023411a9abceec
Filip Dominec <filip.dominec@protonmail.com>
2023-08-26 19:16:22 +0200

    steppers tested on real device, small tweaks

--- example_stepper.py
+++ example_stepper.py
@@ -9,9 +9,9 @@ rp = rp2daq.Rp2daq()       # tip: you can use required_device_id='42:42:42:42:42
 
 ## 1.  Define tuples of coordinates the steppers will go to
 ##     (note we [mis]use mutable data types to act as global variables)
-repeat_cycles = 10
+repeat_cycles = 1
 coords_to_go = [(3,2), (2,5), (1,1)] * repeat_cycles 
-
+coords_to_go += [(0,0)] # finally return back to origin
 
 ## 2.  Initialize steppers, remember the numeric position they are assigned. Your gpio assignments may vary.
 ##     As a practical example, here we implement stepper grouping: The steppers within a group move synchronously, 
@@ -19,21 +19,21 @@ coords_to_go = [(3,2), (2,5), (1,1)] * repeat_cycles
 ##     for multi-axis motion. Note that this script could define several independent stepper groups if needed.
 zero_pos = {}
 steppers_group_bitmask = (1<<0) | (1<<1) 
-zero_pos[0] = rp.stepper_init(0, dir_gpio=12, step_gpio=13, endswitch_gpio=19, inertia=90)["initial_nanopos"]
-zero_pos[1] = rp.stepper_init(1, dir_gpio=10, step_gpio=11, endswitch_gpio=18, inertia=90)["initial_nanopos"]
+zero_pos[0] = rp.stepper_init(0, dir_gpio=12, step_gpio=13, endswitch_gpio=19, inertia=190)["initial_nanopos"]
+zero_pos[1] = rp.stepper_init(1, dir_gpio=10, step_gpio=11, endswitch_gpio=18, inertia=190)["initial_nanopos"]
 #zero_pos[2] = rp.stepper_init(2, dir_gpio=14, step_gpio=15, endswitch_gpio=17, inertia=30)["initial_nanopos"]
 #zero_pos[3] = rp.stepper_init(3, dir_gpio=21, step_gpio=20, endswitch_gpio=16, inertia=30)["initial_nanopos"]
 all_steppers_done = threading.Event() # a thread-safe semaphore
 
 
-## 3. Define callback that feeds both steppers with new coords - but only if all in the group have finished a move!
+## 3. Define callback that feeds both steppers with new coords - but only if both (whole group) have finished a move!
 def stepper_cb(**kwargs):
     ## i. End switches are useful for position calibration (and to prevent dangerous crashes later)
     if kwargs['endswitch_triggered']:
         if kwargs['endswitch_expected']:
             zero_pos[kwargs["stepper_number"]] = kwargs["nanopos"]  # recalibrate position against endstop
         else:
-            print("Error: Unexpected endswitch trigger on motor #{kwargs['stepper_number']}!") 
+            print(f"Error: Unexpected endswitch trigger on motor #{kwargs['stepper_number']}!") 
             coords_to_go[:] = []   # for safety, we flush further movements, so this script ends
 
     ## ii. Check if whole stepper group is done
@@ -42,12 +42,13 @@ def stepper_cb(**kwargs):
 
     ## iii. Feed whole stepper group with new coordinates to go
     try:
+        print(coords_to_go)
         new_coords = coords_to_go.pop(0)
         for st in (0,1):
-            print(f'Stepper {st} now moving to position {new_coords[st]*400000} (nanosteps)')
+            print(f'Stepper {st} now moving to position {new_coords[st]*200000} (nanosteps)')
             rp.stepper_move(st, 
-                    to=zero_pos[st]+new_coords[st] *400000, 
-                    speed=128*(2+5*st), 
+                    to=zero_pos[st]+new_coords[st] * 200000, 
+                    speed=64*(1), 
                     _callback=stepper_cb)
     except IndexError: # i.e. if the last move finished
         all_steppers_done.set()
@@ -55,11 +56,11 @@ def stepper_cb(**kwargs):
 
 ## 3.  Launch the first move for each stepper here (further moves will be launched within the callback)
 ##      Typical larger steppers have 1.8 deg/step, i.e. 200 steps per revolution, therefore 
-##      one revolution corresponds to 16 microsteps x 256 "nanosteps" =
-##      one revolution in 819200 nanosteps
+##      one step corresponds to 16 microsteps x 256 "nanosteps" =
+##      one revolution is thus 819200 nanosteps
 ##      Speed of rotation is given in nanosteps per 100 us cycle; i.e. nanospeed 82 is 1 revolution / s.
 for st in (0,1):
-    rp.stepper_move(st, to=zero_pos[0]-10000000, speed=128*2, _callback=stepper_cb) # moving towards 0 = seeking end switch
+    rp.stepper_move(st, to=zero_pos[0]-10000, speed=28, _callback=stepper_cb) # moving towards 0 = seeking end switch
     print(f'Stepper {st} initiated from the main thread'); time.sleep(.05)
 
 
