e15594d4d7eb1f40041e95ffe41bccebe8439b76
Filip Dominec <filip.dominec@protonmail.com>
2024-04-05 11:33:57 +0200

    search for C files indep of pythonpath

--- example_pwm.py
+++ example_pwm.py
@@ -8,10 +8,13 @@ rp = rp2daq.Rp2daq()
 
 # optimum settings to control a small servo (190 Hz) 
 # typically the timing range is 0.8 .. 2.2 ms pulse width, but YMMV
-gpio = 0
-rp.pwm_configure_pair(gpio=gpio, wrap_value=65535, clkdiv=20, clkdiv_int_frac=0)
+
+rp.pwm_configure_pair(gpio=14, wrap_value=65535, clkdiv=50, clkdiv_int_frac=0)
+#rp.pwm_set_value(gpio=14, value=65534) # minimum position
+rp.pwm_set_value(gpio=14, value=0) # minimum position
+
+gpio = 15
+rp.pwm_configure_pair(gpio=gpio, wrap_value=65535, clkdiv=50, clkdiv_int_frac=0)
 rp.pwm_set_value(gpio=gpio, value=100) # minimum position
-time.sleep(1)
-rp.pwm_set_value(gpio=gpio, value=27500) # maximum position
 
 
