e91a907251193a9161e69f42778cb798ef2c1694
Filip Dominec <filip.dominec@protonmail.com>
2024-07-17 18:14:55 +0200

    sync

--- example_stepper.py
+++ example_stepper.py
@@ -8,10 +8,10 @@ import rp2daq
 rp = rp2daq.Rp2daq()       # tip: you can use required_device_id='42:42:42:42:42:42:42:42'
 
 zero_pos = {}
-zero_pos[0] = rp.stepper_init(0, dir_gpio=14, step_gpio=15, endswitch_gpio=0, inertia=500)["initial_nanopos"] 
+zero_pos[0] = rp.stepper_init(0, dir_gpio=14, step_gpio=15, endswitch_gpio=0, inertia=450)["initial_nanopos"] 
 result = rp.stepper_move(0, 
-        to=zero_pos[0] - 500_000, 
-        speed=1600, 
+        to=zero_pos[0] + 14000_000, 
+        speed=160, 
         endswitch_ignore=0
         ) 
 
